fd0b869c7cc28b2c48a89989b13f0bb1ce87a126
Andrea Grandi <a.grandi@gmail.com>
2014-08-26 14:31:47 +0100

    Fix some bugs. Improve error handling. More verbose output.

--- ghangelog.go
+++ ghangelog.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
 	"os"
 	"os/exec"
@@ -33,19 +35,19 @@ type PayLoad struct {
 }
 
 var configuration = Configuration{}
+var output bytes.Buffer
 
 func parseGhPost(rw http.ResponseWriter, request *http.Request) {
 	// When a POST is made, read the content and unmarshal it to a struc
+	fmt.Println("Decoding new payload ...")
 	payload_decoder := json.NewDecoder(request.Body)
 
 	var payload PayLoad
 	err_payload := payload_decoder.Decode(&payload)
 
 	if err_payload != nil {
-		panic(err_payload)
+		log.Fatalf("Error decoding payload: %s", err_payload)
 	}
-
-	fmt.Println(payload)
 
 	// if state is "closed" check "merge_commit_sha" if it's empty or not
 	if payload.Request.State == "closed" {
@@ -61,20 +63,35 @@ func parseGhPost(rw http.ResponseWriter, request *http.Request) {
 				// Wiki folder doesn't exists, we will have to clone from git
 				clone_command := fmt.Sprintf("git clone https://%s:%s@%s",
 					configuration.GitUsername, configuration.Token, configuration.WikiGitUrl)
-				exec.Command("sh", "-c", clone_command)
+				fmt.Printf("Executing: %s\n", clone_command)
+				cmd := exec.Command("sh", "-c", clone_command)
+				cmd.Stdout = &output
+				err := cmd.Run()
+
+				if err != nil {
+					log.Fatalf("Error cloning git repository (%s): %s", clone_command, err)
+				}
 			} else {
 				// Wiki folder exists, we will have to pull to get updates
 				pull_command := fmt.Sprintf("git -C %s pull", configuration.WikiPath)
-				exec.Command("sh", "-c", pull_command)
+				fmt.Printf("Executing: %s\n", pull_command)
+				cmd := exec.Command("sh", "-c", pull_command)
+				cmd.Stdout = &output
+				err := cmd.Run()
+
+				if err != nil {
+					log.Fatalf("Error pulling git repository (%s): %s", pull_command, err)
+				}
 			}
 
 			// Check the VersionUrl and get the current released version of the software.
 			// This number will be used to determinate if the current version is already in the
 			// wiki changelog or if we have to add a new version section.
+			fmt.Printf("Getting current version from %s ...\n", configuration.VersionUrl)
 			version_resp, err := http.Get(configuration.VersionUrl)
 
 			if err != nil {
-				panic(err)
+				log.Fatalf("Error getting current version: %s", err)
 			}
 
 			defer version_resp.Body.Close()
@@ -82,21 +99,58 @@ func parseGhPost(rw http.ResponseWriter, request *http.Request) {
 			version := strings.Split(string(version_content), "\n")[0]
 
 			// open fileToUpdate, read all the text and split by lines
+			fmt.Printf("Reading from %s ...\n", configuration.WikiChangelogPath)
 			content, err := ioutil.ReadFile(configuration.WikiChangelogPath)
 
 			if err != nil {
-				panic(err)
+				log.Fatalf("Error reading from %s: %s", configuration.WikiChangelogPath, err)
 			}
 
 			wiki_lines := strings.Split(string(content), "\n")
 
 			// Check if the released version is the same we have at the beginning of the wiki
+			fmt.Println("Parsing wiki lines ...")
 			if wiki_lines[0] == fmt.Sprintf("## %s", version) {
 				// Find the first empty line and insert the body_lines there
 				for ln, line := range wiki_lines {
 					if line == "" {
 						wiki_lines = append(wiki_lines[:ln],
 							append(body_lines, wiki_lines[ln:]...)...)
+
+						fmt.Printf("Writing changes to %s ...\n", configuration.WikiChangelogPath)
+						wiki_output := []byte(strings.Join(wiki_lines, "\n"))
+						err := ioutil.WriteFile(configuration.WikiChangelogPath, wiki_output,
+							os.ModeAppend)
+
+						if err != nil {
+							log.Fatalf("Error writing to %s: %s", configuration.WikiChangelogPath,
+								err)
+						}
+
+						// Commit the edited wiki
+						commit_command := fmt.Sprintf("git -C %s commit -a -m '%s'",
+							configuration.WikiPath, payload.Request.Title)
+						fmt.Printf("Executing: %s\n", commit_command)
+						cmd := exec.Command("sh", "-c", commit_command)
+						cmd.Stdout = &output
+						err = cmd.Run()
+
+						if err != nil {
+							log.Fatal("Error in git (%s): %s", commit_command, err)
+						}
+
+						// Push the wiki page on GitHub
+						push_command := fmt.Sprintf("git -C %s push origin master",
+							configuration.WikiPath)
+						fmt.Printf("Executing: %s\n", push_command)
+						cmd = exec.Command("sh", "-c", push_command)
+						cmd.Stdout = &output
+						err = cmd.Run()
+
+						if err != nil {
+							log.Fatal("Error in git (%s): %s", push_command, err)
+						}
+
 						break
 					}
 				}
@@ -105,23 +159,23 @@ func parseGhPost(rw http.ResponseWriter, request *http.Request) {
 				// Add a new section with the new version at the beginning of the file
 				fmt.Println(body_lines)
 			}
-
-			fmt.Println(wiki_lines)
 		}
 	}
 }
 
 func main() {
 	// Read configuration from config.json
+	fmt.Println("Loading configuration from config.json ...")
 	file, _ := os.Open("config.json")
 	decoder := json.NewDecoder(file)
 	err := decoder.Decode(&configuration)
 
 	if err != nil {
-		fmt.Println("error:", err)
+		log.Fatalf("Error reading from config.json: %s", err)
 	}
 
 	// Listen to the given host:port
+	fmt.Printf("Listening on %s:%d\n", configuration.Host, configuration.Port)
 	http.HandleFunc("/", parseGhPost)
 	http.ListenAndServe(fmt.Sprintf("%s:%d", configuration.Host, configuration.Port), nil)
 }
